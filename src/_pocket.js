f4adadbba086791e2de18158e3080c9e6443b235
kPherox <admin@mail.kr-kp.com>
2021-10-20 17:50:23 +0900

    bump mylistpocket version: 0.5.15-fix-mylist-api.2
    deploy to dist

--- src/_pocket.js
+++ src/_pocket.js
@@ -26,7 +26,7 @@
 // @exclude     *://dic.nicovideo.jp/p/*
 // @exclude     *://ext.nicovideo.jp/thumb/*
 // @exclude     *://ext.nicovideo.jp/thumb_channel/*
-// @version     0.5.15-fix-mylist-api.1
+// @version     0.5.15-fix-mylist-api.2
 // @grant       none
 // @author      segabito macmoto
 // @license     public domain
